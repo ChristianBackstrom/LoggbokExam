aab661a87f2e65774422b8b8edb9ea06d87bb395
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-12-01 13:06:54 +0100

    body and user added

---
+++ src/Body.java
@@ -0,0 +1,21 @@
+public class Body {
+    User creator;
+    String text;
+
+    public Body(User creator, String text) {
+        this.creator = creator;
+        this.text = text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCreator() {
+        return this.creator.getUser();
+    }
+
+    public String getText() {
+        return text;
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    String user;
+
+    public User(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+}
