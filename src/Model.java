cafe225c5dc233bc963d3730e22c60d805579fa5
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-12-07 11:13:14 +0100

    Model starting

---
+++ src/Model.java
@@ -0,0 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Model {
+    private Body currentBody;
+    private Save save;
+    private MVC view;
+
+    public Model(Save save){
+        this.view = new MVC();
+        this.view.addLoadListener(new loadListener());
+        this.view.addSaveListener(new saveListener());
+        this.currentBody = view.getCurrentLogg();
+        this.save = new Save();
+
+    }
+
+    public void saveCurrent(){
+        save.saveBody(this.currentBody);
+    }
+
+    private class loadListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            view.setLogg(save.GetSave(Integer.parseInt(view.getLoadNumber())));
+        }
+    }
+
+    private class saveListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            saveCurrent();
+        }
+    }
+}
