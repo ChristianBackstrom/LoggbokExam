4143fcd048c335ded30e7d3ee1ffd0b1c1f39c36
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-12-14 10:28:09 +0100

    style: Removed main method from database.java

--- src/database.java
+++ src/database.java
@@ -4,38 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class database {
-    public static void main(String[] args) {
-        try {
-            // Set up connection to database
-            Connection conn = DriverManager.getConnection(
-                    "jdbc:mysql://" + databaseConfig.DBURL + ":" + databaseConfig.port + "/" + databaseConfig.DBname +
-                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
-                    databaseConfig.user, databaseConfig.password);
-
-            // Setup statement
-            Statement stmt = conn.createStatement();
-            Scanner tgb = new Scanner(System.in);
-            int currentRoom = 1;
-
-            // Create query and execute
-                String strSelect = "select body from logg";
-                //System.out.println("The SQL statement is: " + strSelect + "\n");
-
-                ResultSet rset = stmt.executeQuery(strSelect);
-
-                // Loop through the result set and print
-                //System.out.println("The records selected are:");
-                while (rset.next()) {
-                    String body = rset.getString("body");
-                    System.out.println(body);
-                }
-            // Close conn and stmt
-            conn.close();
-            stmt.close();
-        } catch(SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
 
     public Body getData(int i) throws SQLException {
 
