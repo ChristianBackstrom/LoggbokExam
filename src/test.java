8d88d11cfaaec248b29a35309b1a02decd75eda1
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2021-01-18 10:30:36 +0100

    database config added, test also added

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class test {
-    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+    public static void main(String[] args){
         Model model = new Model();
     }
 }
